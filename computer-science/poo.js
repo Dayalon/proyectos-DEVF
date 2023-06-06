5381a9e24495efff9cfddbd086778488cccf2aa0
dayallara <dayal.lara1988@gmail.com>
2023-06-05 21:38:32 -0600

    inicio POO

---
+++ computer-science/poo.js
@@ -0,0 +1,12 @@
+class Aminal {
+
+    constructor("nombre", "color", "comida"); {
+        this.nombre = nombre
+        this.color = color
+        this.comida = comida
+    }
+}
+
+
+const perro = new Aminal("Huesos", "Café", "carne")
+const gato = new Aminal("Huesos", "Café", "carne")
