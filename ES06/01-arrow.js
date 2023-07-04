46748a7f23c90d6195904050d1d4d2fce19376a8
dayallara <dayal.lara1988@gmail.com>
2023-07-03 21:25:52 -0600

    primer arrowfunction

---
+++ ES06/01-arrow.js
@@ -0,0 +1,32 @@
+function suma (a,b){
+    return a + b
+}
+
+console.log("La suma uno es", suma (30,50));
+
+const sumaDos = function (a,b){
+    return a + b
+}
+
+console.log("La suma dos es", sumaDos (60,50));
+
+function sumaTres (a,b){
+    return a + b
+}
+var num1 = 3
+var num2 = 5
+
+console.log("La suma tres es", sumaTres (num1,num2));
+
+/* Arrow functions */
+/* Azúcar Sintáctivo (Sugar Syntax) */
+
+const sumaCuatro = (a,b) =>{
+    return a + b
+}
+
+console.log("La suma cuatro es", sumaCuatro (60,50));
+
+
+
+
